18b983c76e281656068f58669c3bff3381f703f3
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-03 14:17:52 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,7 +6,5 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  BASE_HOA[:chipmunks] << "Dave"
-  BASE_HOA[:third_eaters] << "Snarf"
-  BASE_HOA[:jetsons] << "Astro"
+  BASE_HOA[:show] << name
 end 
